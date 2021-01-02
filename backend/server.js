9ef52aca446ebbe28ebf9ea6f7cebe0c0102ebb5
habibwebdev <abbasali211@gmail.com>
2021-01-02 18:25:08 +0500

    ENV variables, nodemon, concurrently setup

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,14 @@ const app = express()
 
 // Routes
 app.get('/', (req, res) => {
-  res.send('API is running')
+  res.send('API is running...')
 })
 
-app.listen(5000, console.log('Server is running on port 5000'))
+const port = process.env.PORT || 5000
+
+app.listen(
+  port,
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on http://localhost:${port}`
+  )
+)
